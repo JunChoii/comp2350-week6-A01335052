819579621e3cc8743cb6333b7d5eacea8596583b
JunChoi <97496821+JunChoii@users.noreply.github.com>
2023-02-23 23:26:27 -0800

    Add files via upload

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,29 +1,27 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfigQoddi = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+  host: "sql.freedb.tech",
+  user: "freedb_junissexy",
+  password: "D2QGXfPpU3aq*aY",
+  database: "freedb_comp2350-week4-A01335052",
+  multipleStatements: false,
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "database1",
-	multipleStatements: false
+  host: "localhost",
+  user: "root",
+  password: "Password",
+  database: "database1",
+  multipleStatements: false,
 };
 
 if (is_qoddi) {
-	var database = mysql.createPool(dbConfigQoddi);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+  var database = mysql.createPool(dbConfigQoddi);
+} else {
+  var database = mysql.createPool(dbConfigLocal);
 }
 
 module.exports = database;
-		
